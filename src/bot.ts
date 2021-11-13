f36411e86413bdeecbe7ea2cdacffdd59607de54
knoxiiw <knoxiiw@gmail.com>
2021-11-12 23:13:30 -0500

    bot will no longer acknowledge other bots' messages

--- src/bot.ts
+++ src/bot.ts
@@ -18,6 +18,8 @@ export class Bot {
 
 	public listen(): Promise<string> {
 		this.client.on('messageCreate', (message: Message) => {
+			if (message.author.bot)
+				return;
 			console.log("message received, contents: " + message.content);
 		});
 		return this.client.login(this.token);
