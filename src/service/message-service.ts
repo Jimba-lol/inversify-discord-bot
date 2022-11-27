6a47bb1e1954ee9aa91e485c47ba4a45203a1f19
Jim Knox <knoxiiw@gmail.com>
2022-11-27 16:50:02 -0500

    remove unnecessary resource

--- src/service/message-service.ts
+++ src/service/message-service.ts
@@ -12,7 +12,8 @@ export class MessageService {
 	public handleMessage(message: Message) {
 		// pfft, I'm not reading *that*
 		if (message.content.length >= 800) {
-			message.channel.send({ files: [path.join(this.resourcePath, 'video/meme/duke.mp4')]});
+			// message.channel.send({ files: [path.join(this.resourcePath, 'video/meme/duke.mp4')]});
+			message.channel.send("https://fxtwitter.com/GetGianni/status/1435302082098212866");
 			return;
 		}
 	}
