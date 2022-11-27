7e07bd849f826e301c14c99e9fb41fe425a0ce77
Jim Knox <knoxiiw@gmail.com>
2022-11-27 16:44:21 -0500

    add 'starting up' console message

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { SYMBOLS } from './symbols';
 import { Bot } from './bot';
 
 let bot = container.get<Bot>(SYMBOLS.Bot);
+console.log('Starting up...');
 bot.listen().then((res) => {
 	console.log('Logged in');
 }).catch((error) => {
